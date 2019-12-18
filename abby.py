402dd6fd858eaaf2830e2cd3adda778d26e5fd01
liuliu <851415105@qq.com>
2019-12-18 15:05:34 +0800

    add

--- abby.py
+++ abby.py
@@ -1,1 +1,20 @@
 # 人生苦短，为我happy
+"""
+git branch  #查看分支
+
+#新建分支levi_dev
+git branch levi_dev
+
+#合并分支文件
+git merge levi_dev
+
+#查看当前分支状态
+git status
+
+#切换到分支levi_dev
+git checkout levi_dev
+
+#删除分支levi_dev
+#(-d 删除已合并的分支 -D 删除未合并分支)
+git branch -d levi_dev
+"""
---
+++ levi.py
@@ -0,0 +1,3 @@
+# 你很好，我比较欣赏你哟！
+cai='caiwoshishui'
+hebing=''
