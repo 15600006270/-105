52920ba7f230635cc8c7abe406da32f6b733c7b5
liuliu <851415105@qq.com>
2019-12-18 16:36:04 +0800

    add

--- abby.py
+++ abby.py
@@ -1,6 +1,7 @@
 # 人生苦短，为我happy
-"""
-git branch  #查看分支
+
+#查看分支
+git branch
 
 #新建分支levi_dev
 git branch levi_dev
@@ -11,10 +12,16 @@ git merge levi_dev
 #查看当前分支状态
 git status
 
+#提交到缓存区
+git add *
+
+#提交到本地仓库
+git commit -m 'add'
+git commit -m 'abby work'
+
 #切换到分支levi_dev
 git checkout levi_dev
 
 #删除分支levi_dev
 #(-d 删除已合并的分支 -D 删除未合并分支)
 git branch -d levi_dev
-"""
--- levi.py
+++ levi.py
@@ -1,3 +1,12 @@
 # 你很好，我比较欣赏你哟！
 cai='caiwoshishui'
 hebing=''
+"""
+…或在命令行上创建新的存储库
+回声“＃-105” >> README.md 
+git init 
+git add README.md 
+git commit -m“第一次提交” 
+git remote add origin https://github.com/liuliu6270/-105.git
+ git push -u origin master
+"""
